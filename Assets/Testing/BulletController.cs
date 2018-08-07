e35f351eb2c57f349f879aaf1496535b145b18c9
Tyrel Cameron <s3394330@student.rmit.edu.au>
2018-08-08 09:58:59 +1000

    Created gun for testing

---
+++ Assets/Testing/BulletController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public GameObject explosion;
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Collision");
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
