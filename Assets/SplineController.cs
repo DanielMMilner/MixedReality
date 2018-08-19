c86b532b6983553bd54ece9a90ff5e248090df05
Tyler Watkins <s3542686@student.rmit.edu.au>
2018-08-19 21:27:49 +1000

    New curve editor, add gun

---
+++ Assets/SplineController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BansheeGz.BGSpline.Curve;
+using BansheeGz.BGSpline.Components;
+
+[RequireComponent(typeof(BGCcMath))]
+public class SplineController : MonoBehaviour {
+    private BGCcMath mathCurve;
+
+    public GameObject objToMove;
+
+    public float speed = 1;
+    public float distance = 0;
+
+    public float lookPointDist = 10;
+    private Vector3 lookPoint;
+
+    // Use this for initialization
+    void Start () {
+        mathCurve = GetComponent<BGCcMath>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void FixedUpdate()
+    {
+        //d = s * t
+        distance += speed * Time.fixedDeltaTime;
+
+        lookPoint = mathCurve.CalcPositionByDistance(distance + lookPointDist);
+        objToMove.transform.position = mathCurve.CalcPositionByDistance(distance);
+
+        var heading = lookPoint - objToMove.transform.position;
+        objToMove.transform.rotation = Quaternion.LookRotation(heading);
+
+        Debug.DrawLine(lookPoint, objToMove.transform.position, Color.red);
+
+    }
+
+}
---
+++ Assets/TurretController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TurretController : MonoBehaviour {
+
+    private GameObject player;
+
+    public float range = 100;
+
+    // Time in seconds
+    public float cooldownTime = 5;
+
+    public GameObject bullet;
+    
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Raycast to object
+        // Determine distance between objects
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if(distance < range)
+        {
+            Debug.DrawLine(transform.position, player.transform.position, Color.red);
+            if(cooldownTime <= 0)
+            {
+                // Fire
+
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, player.transform.position, Color.green);
+        }
+        cooldownTime -= Time.fixedDeltaTime;
+	}
+}
