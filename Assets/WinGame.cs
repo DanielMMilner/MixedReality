ca2c888d526c4b0b7274425f2792290f93a55d92
Daniel Milner <danielmmilner@gmail.com>
2018-10-06 14:49:25 +1000

    Added end game win screen

---
+++ Assets/WinGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour {
+    public GameObject canvas; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("PlayerShip"))
+        {
+            canvas.SetActive(true);
+        }
+    }
+}
