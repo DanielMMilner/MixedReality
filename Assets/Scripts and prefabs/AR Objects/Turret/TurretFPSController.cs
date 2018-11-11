83178d9444cb9c9dbdaefaab22ccca31a1405a98
Daniel Milner <danielmmilner@gmail.com>
2018-11-11 14:54:29 +1100

    Fixed turret rotation issue

--- Assets/Scripts and prefabs/AR Objects/Turret/TurretFPSController.cs
+++ Assets/Scripts and prefabs/AR Objects/Turret/TurretFPSController.cs
@@ -6,12 +6,25 @@ public class TurretFPSController : MonoBehaviour
     [SerializeField] private UnityStandardAssets.Characters.FirstPerson.MouseLook m_MouseLook;
 
     public GameObject cannon;
+    public Transform shipParentTransform;
+
     public float cannonRotationSpeed;
     public float maxGunAngle;
     public float minGunAngle;
 
-    public float maxCannonAngle;
-    public float minCannonAngle;
+    public float cannonRotationAngle;
+
+    private Quaternion rightCannonRotation;
+    private Quaternion leftCannonRotation;
+
+    public void Start()
+    {
+        rightCannonRotation = transform.localRotation;
+        leftCannonRotation = transform.localRotation;
+        
+        rightCannonRotation *= Quaternion.Euler(0, cannonRotationAngle, 0); // this adds a 45 degrees Y rotation
+        leftCannonRotation *= Quaternion.Euler(0, -cannonRotationAngle, 0); // this adds a -45 degrees Y rotation
+    }
 
     // Update is called once per frame
     private void Update()
@@ -23,8 +36,8 @@ public class TurretFPSController : MonoBehaviour
     {
         float vertical = CrossPlatformInputManager.GetAxis("Vertical");
 
-        if (vertical == 0)
-            vertical = CrossPlatformInputManager.GetAxis("TurretTurnOtherVertical"); ;
+        if (vertical == 0) //alternative value used when playing with a mouse and keyboard
+            vertical = CrossPlatformInputManager.GetAxis("TurretTurnOtherVertical");
 
         if (vertical > 0 && cannon.transform.right.y < minGunAngle)
         {
@@ -36,16 +49,15 @@ public class TurretFPSController : MonoBehaviour
 
         float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        if (horizontal == 0)
-            horizontal = CrossPlatformInputManager.GetAxis("TurretTurnOtherHorizontal"); ;
+        if (horizontal == 0) //alternative value used when playing with a mouse and keyboard
+            horizontal = CrossPlatformInputManager.GetAxis("TurretTurnOtherHorizontal");
 
-        if (horizontal > 0 && transform.forward.x < minCannonAngle)
+        if (horizontal > 0 && -transform.localRotation.y > -rightCannonRotation.y)
         {
             transform.Rotate(0, cannonRotationSpeed, 0);
-        }
-        else if (horizontal < 0 && transform.forward.x > -maxCannonAngle)
+        }else if (horizontal < 0 && -transform.localRotation.y < -leftCannonRotation.y)
         {
             transform.Rotate(0, -cannonRotationSpeed, 0);
-        }
+        }        
     }
 }
